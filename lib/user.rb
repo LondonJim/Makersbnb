1df0fe76a433f8630d63b2b4d85d1586eea1ff25
Harry <harryjp1995@gmail.com>
2018-09-10 19:48:36 +0100

    Adds basic user class, able to sign up

---
+++ lib/user.rb
@@ -0,0 +1,57 @@
+require 'pg'
+require 'bcrypt'
+
+class User
+  attr_reader :id, :name, :handle, :email, :password
+  def initialize(id:, name:, handle:, email:, password:)
+    @id = id
+    @name = name
+    @handle = handle
+    @email = email
+    @password = password
+  end
+
+  def self.sign_up(name:, handle:, email:, password:)
+    encrypted_pword = BCrypt::Password.create(password)
+    result = env_check.exec("
+      INSERT INTO users (name, handle, email, password)
+      VALUES('#{name}', '#{handle}', '#{email}', '#{encrypted_pword}')
+      RETURNING id, name, handle, email, password
+      ")
+    user_create(result)
+  end
+
+  def self.all
+    users = env_check.exec("SELECT * FROM users")
+    users.map do |user|
+      User.new(
+        id: user['id'],
+        name: user['name'],
+        handle: user['handle'],
+        email: user['email'],
+        password: user['password']
+      )
+    end
+  end
+
+  private
+
+  def self.env_check
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+  end
+
+  def self.user_create(result)
+    user = User.new(
+      id: result[0]['id'],
+      name: result[0]['name'],
+      handle: result[0]['handle'],
+      email: result[0]['email'],
+      password: result[0]['password']
+    )
+    user
+  end
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,9 +3,18 @@ require 'user'
 describe User do
   describe '.sign_up' do
     it 'allows a user to sign up' do
-      new_user = User.sign_up(name: 'XX', user_name: 'XX', email: 'XX', password: 'XX')
+      new_user = User.sign_up(name: 'XX', handle: 'XX', email: 'XX', password: 'XX')
       expect(User.all.first.name).to eq('XX')
-      expect(User.all.first.user_name).to eq('XX')
+      expect(User.all.first.handle).to eq('XX')
+      expect(User.all.first.email).to eq('XX')
+    end
+  end
+
+  describe '.all' do
+    it 'returns info for all users' do
+      User.sign_up(name: 'XX', handle: 'XX', email: 'XX', password: 'XX')
+      expect(User.all.first.name).to eq('XX')
+      expect(User.all.first.handle).to eq('XX')
       expect(User.all.first.email).to eq('XX')
     end
   end
