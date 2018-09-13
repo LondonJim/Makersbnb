9764ba49b614b62fd90a0628e58b46921f5e5539
Harry <37018462+hjpugh@users.noreply.github.com>
2018-09-13 11:57:49 +0100

    Update user_spec.rb
    Keeps tests that are still valid; accounts for the fact that there is now a User.sign_up method

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -14,4 +14,27 @@ describe User do
       expect(User.login(handle: 'XX', password: 'YY')).to be false
     end
   end
+  
+   describe '.sign_up' do	
+    it 'allows a user to sign up' do	
+      User.sign_up(
+        name: 'XX', 
+        handle: 'XX', 
+        email: 'XX', 
+        password: 'XX', 
+        password_confirmation: 'XX'
+        )	
+      expect(User.all.first.name).to eq('XX')	
+      expect(User.all.first.handle).to eq('XX')	
+      expect(User.all.first.email).to eq('XX')	
+    end	
+     it 'does not allow the same handle to sign up again' do	
+      sign_up_user	
+      expect(User.sign_up(name: 'YY', handle: 'XX', email: 'YY', password: 'YY')).to be(nil)	
+    end	
+     it 'does not allow the same email to sign up again' do	
+      sign_up_user	
+      expect(User.sign_up(name: 'YY', handle: 'YY', email: 'XX', password: 'YY')).to be(nil)	
+    end	
+  end
 end
