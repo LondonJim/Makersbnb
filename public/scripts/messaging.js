7c038effcaca17d88cb092bbc1b82029d3790436
James Sutherland <jrsutherland78@googlemail.com>
2018-09-14 12:20:39 +0100

    selected unique dates pushed to array

--- public/scripts/messaging.js
+++ public/scripts/messaging.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  var dates = []
   $(".availability").each(function() {
     $(this).on("click", function() {
-      console.log(this.id);
+      $(`#${this.id}`).css( "color", "purple")
+      dates.includes(this.id) ? console.log("dates already added"): dates.push(this.id);
+      console.log(dates);
     });
   });
 });
